2f181693c773f56d53ec15b7aa89525dfb3949f5
ariandto <ariandto@gmail.comgit config --global user.name ariandtogit config --get-all user.email ariandto@gmail.com>
2022-09-03 11:45:17 +0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import db from "./config/database.js";
+import router from "./routes/index.js";
+// setelah table terbuat hapus
+// import Users from './models/userModel.js';
+const app= express();
+
+try {
+    await db.authenticate();
+    console.log('Connected to database MySql....');
+    // setelah table terbuat hapus
+    // await Users.sync();
+} catch (error) {
+    console.error(error);
+}
+app.use(express.json);
+app.use(router);
+const port = process.env.PORT || 8100 ;
+app.listen(port, ()=>console.log('Service running at port '+port));
