2d78e50e1fc0e713b9e28fcb4401cfe10e279dcf
ariandto <92358210+ariandto@users.noreply.github.com>
2022-09-07 11:19:44 +0700

    changes

--- backend/controller/Users.js
+++ backend/controller/Users.js
@@ -1,9 +1,27 @@
 import Users from '../models/userModel.js';
+import bcrypt from "bcrypt";
 
 export const getUsers = async(req, res) => {
     try {
         const users = await Users.findAll();
         res.json(users);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const Register = async(req, res) => {
+    const {name, email, password, confPassword} = req.body;
+    if(password !== confPassword) return res.status(400).json({msg:"Password dan Confirm password tidak cocok"});
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
+    try {
+        await Users.create({
+            name: name,
+            email: email,
+            password: hashPassword
+        });
+        res.json({msg:"Register Akun Berhasil"});
     } catch (error) {
         console.log(error);
     }
