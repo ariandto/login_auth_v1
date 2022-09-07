68127874d274920ccc2829e8e5007493182e35d1
ariandto <92358210+ariandto@users.noreply.github.com>
2022-09-07 11:33:12 +0700

    changes2

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,7 @@ import {getUsers, Register} from "../controller/Users.js";
 const router = express.Router();
 
 router.get('/users', getUsers);
+router.post('/users',Register);
 
 
 export default router;
